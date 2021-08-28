157f3b7b97c279393956455692bab8ff94af492a
alvaropaxql <alvaropasnu@gmail.com>
2021-08-28 12:14:25 +0200

    Blackjack Game: v2.1
    Game starts, renders and adds new cards. Yet to be completed.

--- BlackjackGame/index.js
+++ BlackjackGame/index.js
@@ -1,18 +1,41 @@
 let firstCard = 10
 let secondCard = 11
-let sum = firstCard + secondCard + 4
+let cards = [firstCard, secondCard]
+let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEL = document.getElementById("message-el")
+let sumEL = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el") // let sumEL = document.querySelector("#sum-el") //# is for id attr , . is for class attr
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! 😭"
-    isAlive = false
+function startGame() {
+    renderGame()
 }
 
-console.log(message)
+function renderGame() {
+
+    sumEL.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + cards[0] + " - " + cards[1]
+
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+
+    messageEL.textContent = message 
+}
+
+function renderGame() {
+    let card = 1
+
+    sum += card
+    cards.push(card)
+
+    startGame()
+}
