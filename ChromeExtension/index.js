b4b48ba27570389108251a82d2b8d70d4aba5acd
alvaropaxql <alvaropasnu@gmail.com>
2021-08-30 17:30:02 +0200

    Chrome Extension app: v3.1
    Chrome extension app done and ready to be deployed. It works as a bookmark for tabs and websites.

--- ChromeExtension/index.js
+++ ChromeExtension/index.js
@@ -1,27 +1,53 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el") // const make a variable not be able to be reassigned
 const inputSaveBtn = document.getElementById("input-btn")
+const tabSaveBtn = document.getElementById("tab-btn")
+const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
 
 inputSaveBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
     inputEl.value = ""
-    renderLeads()
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
 })
 
-function renderLeads() {
+tabSaveBtn.addEventListener("click", function () {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
+
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
-        listItems += 
+    for (let i = 0; i < leads.length; i++) {
+        listItems +=
             `<li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>`
-        /*const li = document.createElement("li")
+        /*
+        listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+        *------------------------------------------------------------------------------------------*
+        const li = document.createElement("li")
         li.textContent = myLeads[i]
-        ulEl.append(li)*/
+        ulEl.append(li)
+        */
     }
     ulEl.innerHTML = listItems
 }
--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # Javasacript practices from Scrimba Course
 
-**Web course**: [*Learn JavaSript*](https://scrimba.com/learn/learnjavascript) with Per Harald Borgen
+**Web course**: [*Learn JavaSript*](https://scrimba.com/learn/learnjavascript) with Per Harald Borgen. This is just a way to remember some of the basic JavaScript I have learnt in the past years.
 
 ## *Practices*
 
 Throughout the course, three apps were built on JavaScript. These are the apps:
 
-+ *First practice* : **Passenger counting app**
++ *First practice* : The **Passenger counting app** is just a counter that also shows previous saved records.
 <img src="/PassengerCountingApp/screenshots/passenger_counter.png" alt="Passenger counting app" width="350"/>
-+ *Second practice* : **Blackjack game app**
++ *Second practice* : The **Blackjack game app** is a simple demo of the famous *Blackjack* cards game.
 <img src="/BlackjackGame/screenshots/blackjack.png" alt="Blackjack game app" width="350"/>
-+ *Third practice* : **Chrome extension app**
++ *Third practice* : This **Chrome extension app** lists all tabs and websites you have saved as a bookmark would do.
+<img src="ChromeExtension\screenshots\chrome-ext.png" alt="Chrome extension app" width="700"/>
